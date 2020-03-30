5980f8caf0ccfee65e5f2865ba97182eb85ed8a5
tamus88 <tamus88@mail.ru>
2020-03-30 12:25:34 -0400

    update

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -5,5 +5,6 @@ public class Colors {
     public static void main(String[] args) {
         String[] colors = {"red", "blue", "green", "yellow", "black", "red"};
         System.out.println("My colors");
+
     }
 }
